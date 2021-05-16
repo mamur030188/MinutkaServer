9f84520baf28c9832368d547f50d9843d6e5ee05
Mamirjon <mike030188@gmail.com>
2021-05-16 22:28:50 +0900

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -36,4 +36,39 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (token) {
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if (err) {
+                var err = new Error('You are not authenticated!');
+                err.status = 401;
+                next(err);
+                return;
+            }
+            else {
+                req.decoded = decoded;
+                next(err);
+            }
+        });
+    }
+    else {
+        var err = new Error('no token provided!');
+        err.status = 403;
+        next(err);
+        return;
+    }
+};
+
+exports.verifyAdmin = function (req, res, next) {
+    if (req, user, admin) {
+        next();
+    }
+    else {
+        var err = new Error('YOu are not authorized to perform this operation!');
+        err.status = 403;
+        next(err);
+        return;
+    }
+}
