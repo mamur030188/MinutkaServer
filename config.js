0508bf626c3b4bc111a200584f04d3819344645a
Mamirjon <mike030188@gmail.com>
2021-05-19 20:25:52 +0900

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '832222664357407',
+        clientSecret: 'bfc154e085a200868c0332ad3b1ccafd'
+    }
 }
